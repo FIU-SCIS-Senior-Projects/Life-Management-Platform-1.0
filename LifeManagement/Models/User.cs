42f78bcbe87aa81ee11e51d9a8d9230204eda0d0
fmoje001 <fmoje001@fiu.edu>
2017-05-30 10:54:22 -0400

    added LifeSuccess field

--- LifeManagement/Models/User.cs
+++ LifeManagement/Models/User.cs
@@ -31,6 +31,7 @@ namespace LifeManagement.Models
         public string password { get; set; }
         public System.DateTime DateCreated { get; set; }
         public string Vision { get; set; }
+        public string LifeSuccess { get; set; }
     
         public virtual Role Role { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
