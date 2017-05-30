4ae42ad3445430207e3ed1ebab80db6267d3d6c2
Beatriz Hernandez <bhern104@fiu.edu>
2017-05-30 16:37:33 -0400

    Fixing bugs

--- LifeManagement/Controllers/QuestionnarieController.cs
+++ LifeManagement/Controllers/QuestionnarieController.cs
@@ -7,10 +7,13 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
 
+
 namespace LifeManagement.Controllers
 {
     public class QuestionnarieController : Controller
     {
+        private SeniorDBEntities db = new SeniorDBEntities();
+        
         // GET: Questionnarie
         public ActionResult Index()
         {
@@ -21,7 +24,29 @@ namespace LifeManagement.Controllers
         public bool collect_questionnarie(QuestionarieViewModel data)
         {
 
+            string name = User.Identity.Name;
+            var user = db.Users.Where(atr => atr.username == name).FirstOrDefault();
+            
+            if(user != null)
+            {
+                int id = user.Id;
+            }
 
+            var g1 = new Goal();
+            g1.Description = data.goal_1;
+            g1.
+            newuser.FirstName = fromuser.FirstName;
+            newuser.LastName = fromuser.LastName;
+            newuser.DOB = fromuser.DOB;
+            newuser.Email = fromuser.Email;
+            newuser.username = fromuser.username;
+            newuser.password = fromuser.password;
+            newuser.Vision = "";
+            newuser.LifeSuccess = "";
+            newuser.DateCreated = DateTime.Now;
+            db.Users.Add(newuser);
+            db.SaveChanges();
+            return Constants.MSGS.SUCCESS;
             return true;
         }
 
