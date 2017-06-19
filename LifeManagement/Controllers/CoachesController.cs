a98a7801a59760fa00594fdaee358ce9273bb976
fmoje001 <fmoje001@fiu.edu>
2017-06-19 14:54:39 -0400

    coaches list in angular

--- LifeManagement/Controllers/CoachesController.cs
+++ LifeManagement/Controllers/CoachesController.cs
@@ -1,208 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
 using System.Linq;
-using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using LifeManagement.Models;
-using System.Web.Security;
 
 namespace LifeManagement.Controllers
 {
     public class CoachesController : Controller
     {
         private SeniorDBEntities db = new SeniorDBEntities();
-        private Common common = new Common();
-
-        // GET: Coaches
-        public ActionResult Index()
+       /**************working with coaches fernando******************/
+        public JsonResult GetCoaches()
         {
-            return View(db.Coaches.ToList());
+            var coaches = db.Coaches.ToList();
+            return Json(coaches, JsonRequestBehavior.AllowGet);
         }
 
-        // GET: Coaches/Details/5
-        public ActionResult Details(int? id)
+        public PartialViewResult CoachesList()
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Coach coach = db.Coaches.Find(id);
-            if (coach == null)
-            {
-                return HttpNotFound();
-            }
-            return View(coach);
+            return PartialView();
         }
 
-        // GET: Coaches/Create
-        [Authorize(Roles = "Admin")]
-        public ActionResult Create()
+        public ActionResult SeeCoaches()
         {
             return View();
         }
-
-        // POST: Coaches/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(CoachViewModel coach)
-        {
-            if (ModelState.IsValid)
-            {
-                var role = db.Roles.Where(a => a.Name == Constants.ROLES.COACH).FirstOrDefault();
-
-                if (db.Coaches.Where(a => a.Username.ToLower() == coach.Username.ToLower()).Count() > 0)
-                {
-                    ViewBag.ErrorMsg = "That username is already taken, try another one";
-                    return View(coach);
-                }
-
-                if (role != null)
-                {
-
-                    var coachOb = new Coach();
-                    coachOb.FirstName = coach.FirstName;
-                    coachOb.LastName = coach.LastName;
-                    coachOb.ReviewScore = 5;    //5 by default
-                    coachOb.Biography = coach.Biography;
-                    coachOb.Skills = coach.Skills;
-                    coachOb.Username = coach.Username;
-                    coachOb.Password = coach.Password;
-                    coachOb.RoleId = role.Id;
-
-                    db.Coaches.Add(coachOb);
-                    db.SaveChanges();
-                
-                    return RedirectToAction("DashBoard", "Users");
-                }
-                else
-                {
-                    ViewBag.ErrorMsg = "Authentication error!";
-                    View("Error");
-                }
-            }
-
-            return View();
-        }
-
-        // GET: Coaches/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Coach coach = db.Coaches.Find(id);
-            if (coach == null)
-            {
-                return HttpNotFound();
-            }
-            
-            return View(coach);
-        }
-
-        // POST: Coaches/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,ReviewScore,Biography,Skills,Username,Password,FirstName,LastName,Avatar,AvatarMime, RoleId")] Coach coach)
-        {
-            if (ModelState.IsValid)
-            {
-
-                db.Coaches.Attach(coach);
-
-                var entry = db.Entry(coach);
-                entry.State = EntityState.Modified;
-
-                entry.Property(e => e.RoleId).IsModified = false;
-
-                try { 
-                db.SaveChanges();
-                }
-                catch (Exception e)
-                {
-                    ViewBag.ErrorMsg = "Error! There can not be empty fields";
-                    return View(coach);
-                }
-
-                return RedirectToAction("Index");
-            }
-            return View(coach);
-        }
-
-        // GET: Coaches/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Coach coach = db.Coaches.Find(id);
-            if (coach == null)
-            {
-                return HttpNotFound();
-            }
-            return View(coach);
-        }
-
-        // POST: Coaches/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Coach coach = db.Coaches.Find(id);
-            db.Coaches.Remove(coach);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        public PartialViewResult CoachEditProfile()
-        {
-            var coach = db.Coaches.Where(c => c.Username.ToLower() == User.Identity.Name.ToLower()).FirstOrDefault();
-            return PartialView(coach);
-        }
-
-        [HttpPost]
-        public ActionResult CoachEditProfile(string FirstName, string LastName, string Biography, string Skills)
-        {
-
-
-            var coach = db.Coaches.Where(c => c.Username.ToLower() == User.Identity.Name.ToLower()).FirstOrDefault();
-
-            if ( coach != null)
-            {
-                if (coach.FirstName != FirstName)  coach.FirstName = FirstName;
-                if (coach.LastName != LastName) coach.LastName = LastName;
-                if (coach.Biography != Biography) coach.Biography = Biography;
-                if (coach.Skills != Skills) coach.Skills = Skills;
-
-                if (Request.Files.Count > 0)
-                {
-                    var file = Request.Files[0];
-                    if (common.saveImageBytesCoach(coach, file))
-                    {
-                        coach.Avatar = common.ResizeImageFile(coach.Avatar, 300);
-                    }
-                }
-
-                db.SaveChanges();
-            }
-            return View();
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+        /*************************************************************/
     }
 }
---
+++ LifeManagement/Scripts/Coaches.js
@@ -0,0 +1,22 @@
+﻿var CoachApp = angular.module('CoachApp', []);
+CoachApp.controller('CoachController', function ($scope, CoachService) {
+ 
+    getCoaches();
+    function getCoaches() {
+        CoachService.getCoaches()
+            .then(function(coaches) {
+                $scope.Coaches = coaches;
+                console.log($scope.Coaches);
+            });
+    }
+});
+ 
+CoachApp.factory('CoachService', ['$http', function ($http) {
+ 
+    var CoachService = {};
+    CoachService.getCoaches = function () {
+        return $http.get('/Coaches/GetCoaches');
+    };
+    return CoachService;
+ 
+}]);
