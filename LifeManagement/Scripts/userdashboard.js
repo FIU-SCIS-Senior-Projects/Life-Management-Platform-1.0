a8547d320b256d4214d0f1e2f20ee32338ee96e1
fmoje001 <fmoje001@fiu.edu>
2017-06-21 20:59:21 -0400

    integrated coaches into dashboard

---
+++ LifeManagement/Scripts/userdashboard.js
@@ -0,0 +1,15 @@
+﻿function renderDiv(url, data) {
+  
+    $.ajax({
+        url: url,
+        data: data,
+        type: 'GET',
+        success: function(res) {
+            $("#usersection").html(res);
+        },
+        error: function(msg) {
+            $("#usersection").html(msg);
+        }
+    });
+  
+}
