04a3ade00a098926fbbddedb312a466c913017d3
fmoje001 <fmoje001@fiu.edu>
2017-06-27 14:55:23 -0400

    files section added

---
+++ LifeManagement/Controllers/AttachmentsController.cs
@@ -0,0 +1,68 @@
+﻿using LifeManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LifeManagement.Controllers
+{
+    public class AttachmentsController : Controller
+    {
+        private SeniorDBEntities db = new SeniorDBEntities();
+        // GET: Attachments
+        public FileContentResult GetFile(int id)
+        {
+            var file = db.ForumFiles.Find(id);
+            if (file != null)
+            {
+                 return File(file.FileBytes,file.MimeType);
+            }
+            return null;
+
+        }
+
+        public PartialViewResult FileViewer(int id)
+        {
+          return PartialView(id);
+        }
+        [HttpPost]
+        public bool SaveFile(int forumid)
+        {
+            if (Request.Files.Count > 0)
+            {
+                var file = Request.Files[0];
+                return SaveFile(forumid, file);
+            }
+            return false;
+        }
+        private bool SaveFile(int forumid, HttpPostedFileBase file)
+        {
+            var forum = db.Forums.Find(forumid);
+            if (forum == null)
+                return false;
+        
+            if (file != null)
+            {
+                try
+                {
+                    var newForumFile = new ForumFile();
+                    newForumFile.MimeType = file.ContentType;
+                    newForumFile.FileBytes = new byte[file.ContentLength];
+                    file.InputStream.Read(newForumFile.FileBytes, 0, file.ContentLength);
+                    forum.ForumFiles.Add(newForumFile);
+                    db.SaveChanges();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+             
+
+            }
+            return false;
+        }
+
+    }
+}
--- LifeManagement/Scripts/forums.js
+++ LifeManagement/Scripts/forums.js
@@ -7,6 +7,7 @@ function setupAllUp() {
     coachLinksSetup();
     sendMsgBtnSetup();
     setCoachLinks();
+  
 }
 function sendMsgBtnSetup() {
     $("#sendmessage").click(function() {
@@ -18,16 +19,50 @@ function sendMsgBtnSetup() {
 }
 
 function coachLinksSetup() {
-    debugger;
+   
     $('.coachlink').click(function () { //click event of the link
         $('.coachlinediv').removeClass('coachlink-active');
         $(this).children(".coachlinediv").addClass('coachlink-active');
         var forumid = $(this).attr('forumid');
         $("#forumid").val(forumid);
         renderConvosSection(forumid);
+        renderFileSection(forumid);
         $('.nav-pills a[href="#1b"]').tab('show');
     });
 
+}
+function saveFile() {
+    if ($("#attachment").val() != "") {
+        if (($("#attachment")[0].files[0].size) >= 2097152)       //2MB
+        {
+            alert("File i too big");
+            return;
+        }
+    }
+
+    var forumid = $("#forumid").val();
+    var formdata = new FormData();
+    formdata.append("forumid", forumid);
+    var fileInput = $('#attachment');
+    $.each($(fileInput).get(0).files, function (index, value) {
+        formdata.append(value.name, value);
+    });
+    $.ajax({
+        url: '/Attachments/SaveFile/',
+        type: 'POST',
+        data: formdata,
+        contentType: false,
+        cache: false,
+        processData: false,
+        success: function (res) {
+            renderFileSection(forumid);
+
+        },
+        error: function (msg) {
+
+        }
+    });
+   
 }
 function setCoachLinks() {
   
@@ -58,9 +93,15 @@ function renderConvosSection(forumid) {
 }
 
 function renderCoachesSection() {
-   $(renderChatDiv("/Forums/ConversationUsers", null, "#coachessection")).then( setupAllUp());
+  renderChatDiv("/Forums/ConversationUsers", null, "#coachessection");
    
          }
+
+function renderFileSection(forumid) {
+    var data = { forumid: forumid };
+    renderChatDiv("/Forums/FilesSection", data, "#filessection");
+   
+}
 
 function renderChatDiv(url, data, divid) {
 
