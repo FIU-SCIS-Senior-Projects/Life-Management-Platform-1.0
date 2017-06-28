bfbca80890607ddd01ed8655db07f8a614fc8026
fmoje001 <fmoje001@fiu.edu>
2017-06-28 15:25:43 -0400

    calendar done

---
+++ LifeManagement/Models/CalendarModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LifeManagement.Models
+{
+    public class CalendarModel
+    {
+        public string title { get; set;}
+        public DateTime start { get; set; }
+        public DateTime end { get; set; }
+        public string url { get; set; }
+        public bool allDay { get; set; }
+        public string backgroundColor { get; set; }
+        public string textColor { get; set; }
+        public string description { get; set; }
+        public int apptId { get; set; }
+
+
+    }
+}
