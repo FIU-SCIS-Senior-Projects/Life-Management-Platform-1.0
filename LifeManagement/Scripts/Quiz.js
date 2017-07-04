e72503eb388735ec7c5e5cebf3cb5ca587c01e2e
fmoje001 <fmoje001@fiu.edu>
2017-07-03 20:29:02 -0400

    quiz done

---
+++ LifeManagement/Scripts/Quiz.js
@@ -0,0 +1,25 @@
+﻿function setAnswerLis() {
+    $(".answer-li").click(function() {
+        var question = $(this).attr("question");
+       
+
+        $(".answer-li").each(function() {
+            if ($(this).attr("question") == question) {
+                $(this).removeClass("answer-li-selected");
+            }
+        });
+
+        $(this).addClass("answer-li-selected");
+    });
+}
+
+function seeResult() {
+    var quiztotal = 0;
+    $(".answer-li-selected").each(function() {
+        quiztotal = quiztotal + parseInt($(this).attr("value"));
+    });
+    $("#quiztotal").val(quiztotal);
+}
+$(document).ready(function() {
+    setAnswerLis();
+})
